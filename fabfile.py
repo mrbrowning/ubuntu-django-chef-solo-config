4b8fbfbf00be67d11d45ba0d2ba0e11391f211d5
Michael Browning <mrbrowning@distopia.net>
2013-05-22 12:10:48 -0500

    add push project op, parameterize app name

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,11 @@
 from fabric.api import *
 
 env.user = 'root'
-env.hosts = ['yourhosthere']
-env.cookbooks = '/path/to/ubuntu-django-chef-solo-config/chef/'
-env.chef_executable = '/var/lib/gems/1.8/bin/chef-solo -c /var/chef/config/solo.rb -j /var/chef/config/node.json'
-
+env.hosts = ['192.168.82.131']
+env.local = '192.168.82.1'
+env.app_name = 'app_name'
+env.cookbooks = '/Users/%s/projects/deploy/chef/'
+env.chef_executable = '/usr/local/bin/chef-solo -c /var/chef/config/solo.rb -j /var/chef/config/node.json'
 
 
 def install_environment():
@@ -22,6 +23,16 @@ def install_environment():
     sudo('cd /etc/chef && %s' % env.chef_executable, pty=True)
 
 
+def push_project():
+    sudo('git clone ssh://%s@%s/Users/%s/%s/%s /var/www/%s/%s' % (env.user, env.local, env.user, env.app_name, env.app_name, env.app_name, env.app_name), pty=True)
+    sudo('chown -R %s:www-data /var/www/%s' % (env.user, env.app_name))
+    sudo('mkdir /var/www/%s/%s/log' % (env.app_name, env.app_name))
+    sudo('chown %s:www-data /var/www/%s/%s/log' % (env.user, env.app_name, env.app_name))
+    sudo('chmod g+w /var/www/%s/%s/log' % (env.app_name, env.app_name))
+    sudo('service uwsgi restart')
+    sudo('service nginx restart')
+
+
 def update_environment():
     _sync_config()
     sudo('cd /etc/chef && %s' % env.chef_executable, pty=True)
@@ -29,5 +40,3 @@ def update_environment():
 
 def _sync_config():
     local('rsync --exclude "*.py" --exclude "*.pyc" -av %s %s@%s:/var/chef' % (env.cookbooks, env.user, env.hosts[0]))
-
-    
