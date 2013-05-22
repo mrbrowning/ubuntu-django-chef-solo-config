c5c1bea54532c879037ce4379335515fe9ea8cb0
Michael Browning <mrbrowning@distopia.net>
2013-05-22 12:20:57 -0500

    update python version/hash, add app_name node var

--- chef/cookbooks/python/attributes/default.rb
+++ chef/cookbooks/python/attributes/default.rb
@@ -21,8 +21,10 @@
 default['python']['install_method'] = 'package'
 
 default['python']['url'] = 'http://www.python.org/ftp/python'
-default['python']['version'] = '2.7.1'
-default['python']['checksum'] = '80e387bcf57eae8ce26726753584fd63e060ec11682d1145af921e85fd612292'
-default['python']['prefix_dir'] = '/usr/local'
+default['python']['version'] = '2.7.3'
+default['python']['checksum'] = 'c57477edd6d18bd9eeca2f21add73919'
+default['python']['prefix_dir'] = '/usr/bin'
+default['python']['app_name'] = 'app_name'
+default['python']['venv_dir'] = "/var/www/#{default['python']['app_name']}/venv"
 
 default['python']['configure_options'] = %W{--prefix=#{python['prefix_dir']}}
